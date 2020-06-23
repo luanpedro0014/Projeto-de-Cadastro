00136c826f963e56ffc564bf4f41c020109e0a0c
Luan Pedro <luanpedro0014@gmail.com>
2020-06-23 14:18:37 -0300

    Project Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema_de_cadastro_de_dados.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
